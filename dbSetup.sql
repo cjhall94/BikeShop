d0bc83b5e17d70a71b0282ecdbff9a039f5a75ce
Corey Taylor <ctaylor5@mail.umw.edu>
2017-04-19 19:41:55 +0000

    Last touches to fixup parts of the site.

--- dbSetup.sql
+++ dbSetup.sql
@@ -18,6 +18,7 @@ CREATE TABLE items (
                     stock int,
                     rating int,
                     stockLimit int,
+                    image text DEFAULT 'Default.jpg',
                     PRIMARY KEY  (ID)
                     );
 
@@ -30,12 +31,28 @@ CREATE TABLE users (
                     password VARCHAR(64) NOT NULL
                     );
                             
-
- 
-                            
-                            
 INSERT INTO users VALUES('ecooper@umw.edu','Gusty', 'Cooper', crypt('1', gen_salt('bf')));                            
 INSERT INTO users VALUES('tempEmail@yahoo.com', 'Johnny', 'Boy', crypt('1', gen_salt('bf')));
 INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews, 
     manufacturer, stock, rating, stockLimit) 
     VALUES('Seat', 'Its a seat', 'parts', 10.95, 11.95, 'Fits most butts', 'Feels like heaven', 'ButtsRUs', 4, 3, 6 );
+INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews, 
+    manufacturer, stock, rating, stockLimit) 
+    VALUES('Wheel', 'Its round', 'parts', 14.95, 10.78, 'Circular', 'It goes round and round', 'Infinate Loop', 2, 5, 6 );
+INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews, 
+    manufacturer, stock, rating, stockLimit) 
+    VALUES('Handle Bars', 'You hold them', 'parts', 56.95, 0.78, 'Handy', 'Handles like a dream', 'Hang On', 15, 78, 6 );
+INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews, 
+    manufacturer, stock, rating, stockLimit) 
+    VALUES('Gears', 'They are spikey', 'parts', 0.95, 10.78, 'You need them', 'The kept me going.', 'Gears 4 You', 34, 25, 8 );
+    
+DROP TABLE IF EXISTS blog;
+CREATE TABLE blogs (
+  blog varchar(1000000)  NOT NULL default '',
+  title varchar (1000),
+  author varchar(100) NOT NULL);
+
+INSERT INTO blogs (blog, title, author) VALUES ('I bought a Trek Marlin today, after much deliberation and research. I needed a bike I could take anywhere and hoon around with, and I had a budget of ~$650.
+I made a post here that helped me decide; Huge thanks to everyone for helping me understand what to look for in a hardtail. Ive only ever ridden cheap walmart bikes my whole life, and this bike is a world away from anything Ive touched before. I chose this bike over the other suggestions because I really wanted to support my local bike shop as much as possible, and the other options werent available there.
+Ive left this bike stock as Ive only had it for a day, but Im looking into upgrading the front fork to fox/rockshox in the near future. I do a bit of everything, with a fair amount of my riding occurring on very poorly paved roads, with some off-road shortcuts and exploration rides in between.
+One thing that stood out to me with the advice I received was a lot of stay away from x or dont get anything below y, but if youve only ridden cheap, shoddy walmart bikes, anything even at the base of entry-level will blow you away. I wouldnt change anything about this bike at this price point. I love it. Thanks everyone for getting me into the world of nice bikes.', 'New Bike Day! Trek Marlin 6', 'Sam Smith');
--- server.py
+++ server.py
@@ -2,10 +2,15 @@ import psycopg2
 import psycopg2.extras
 import os
 import sys
+reload(sys)
 import uuid
 import time
+import string
+import cgitb, cgi
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, send, join_room, leave_room
+sys.setdefaultencoding("UTF8")
+
 
 from subprocess import call
 call(["sudo","service","postgresql","start"])
@@ -20,42 +25,46 @@ page = ""
 
 @app.route('/')
 def mainIndex():
-    
+
+
   return render_template('index.html')
 
-    
-@socketio.on('register', namespace='/bikeShop')
-def register(email, pw, cpw, fn, ln):
-  conn = connectToDB()
-  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-  print("getting into socket")
-  if pw == cpw:
-    cur.execute("select * from users WHERE email = %s", (email,))
-    if cur.fetchone():
-      print "Account already registered to that name"
-      emit('registerfailure', "Email already registered!")
-      
-    else:
-      cur.execute("INSERT INTO users (email, password, firstname, lastname) VALUES(%s, crypt(%s, gen_salt('bf')), %s, %s);", (email, pw, fn, ln))
-      conn.commit()
-      
-  else:
-    emit('registerfailure', "Passwords don't match!")
-  
-  
   
 #######################################Finishedish stuff ################################
 def connectToDB():
-#  connectionString = 'dbname=music user=postgres password=PeekoT55! host=localhost'
     connectionString = 'dbname=bikesdb user=postgres password=1234 host=localhost'
     print (connectionString)
     try:
       return psycopg2.connect(connectionString)
     except:
       print("Can't connect to database")
+   
+   
+  
+@app.route('/search', methods=['GET','POST'])
+def search():
+    results = [];
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+   
+    searchTerm = '%' + request.form['Search'] + '%'
+    
+    print searchTerm
+    
+    cmd = cur.execute("select author from blogs where author like %s", (searchTerm,))
+    try:
+      cur.execute("select author from blogs where author like %s", (searchTerm,))
+      results = cur.fetchall()
+      print results
       
-   
-   
+    except:
+      print("Error executing select")
+      print cmd
+      
+    
+    return render_template('search.html', results=results)
+
+
 @app.route('/news', methods=['GET'])
 def news():
   
@@ -65,10 +74,21 @@ def news():
   
 @app.route('/blog', methods=['GET'])
 def blog():
-  
-  return render_template('blog.html')
-  
-  
+    results = [];
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cmd = """select * from blogs;"""
+    try:
+      cur.execute(cmd)
+      results = cur.fetchall()
+      print results
+      
+    except:
+      print("Error executing select category")
+      print cmd
+    return render_template('blog.html', blogs=results)
+    
+
 
 @app.route('/contact', methods=['GET'])
 def contact():
@@ -81,12 +101,83 @@ def shop():
   
   return render_template('shop.html')  
   
+
+@app.route('/layout', methods=['GET', 'POST'])
+def sesh():
+  conn = connectToDB()
+  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  if 'UserEmail' in session:
+    print ("true")
+  else:
+    return redirect(url_for('mainIndex'))
+ 
+  user=[session['UserEmail'],session['UserFirstName'], session['UserLastName']]
+
+  print(user[0])
+  
+  return render_template('layout.html')
+
   
 @app.route('/signup', methods=['GET','POST'])
 def signup():
+  conn = connectToDB()
+  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  print("getting into socket")
+  
+  if request.method == 'POST':
+    email = request.form['email']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    password = request.form['password']
+    confirmpassword = request.form['confirmpassword']
+  
+    if password == confirmpassword:
+      cur.execute("select * from users WHERE email = %s", (email,))
+      if cur.fetchone():
+        print("Account already registered to that name.")
+        print('registerfailure', "Email already registered!")
+      
+      else:
+        cur.execute("INSERT INTO users (email, firstName, lastName, password) VALUES (%s, %s, %s, crypt(%s, gen_salt('bf')));", (email, firstName, lastName, password))
+        conn.commit()
+      
+    else:
+      print('registerfailure', "Passwords don't match!")
+      
+    return redirect(url_for('registered'))
+  
   
   return render_template('signup.html')
   
+
+@app.route('/registered', methods=['GET'])
+def registered():
+  
+  return render_template('registered.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  conn = connectToDB()
+  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  print("Makes it to login")
+  if request.method == 'POST':
+    username = request.form['email']
+    password = request.form['password']
+    
+    cur.execute("select * from users where email = %s and password = crypt(%s, password);", (username, password))
+    row = cur.fetchone()
+    while row is not None:
+      session['UserFirstName'] = row['firstname']
+      session['UserLastName'] = row['lastname']
+      session['UserEmail'] = row['email']
+      print(session['UserFirstName'])
+      return redirect(url_for('sesh'))
+    else:
+      return redirect(url_for('mainIndex'))
+  else:
+    return redirect(url_for('mainIndex'))
+
   
 @app.route('/product', methods=['GET'])
 def product():
@@ -104,20 +195,97 @@ def product():
     #print r['artist']
   return render_template('product.html', items=results)
   
-@app.route('/category/<inCategory>', methods=['GET'])
+@app.route('/addProduct', methods=['GET', 'POST'])
+def addTo():
+    results = []
+    conn = connectToDB()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        form = cgi.FieldStorage()
+        
+        if request.form['submit'] == 'Search':
+                try:
+                  cmd = """SELECT * FROM items WHERE name = '%s';""" % (request.form['name'])
+                  print(cmd)
+                  cur.execute(cmd)
+                  results = cur.fetchall()
+                  
+                  return render_template('addProduct.html', item = results[0])
+                except:
+                  print("Could not find %s" % request.form['name'] )
+                  
+        elif request.form['submit'] == 'Modify Product':
+                try:
+                  cmd = """UPDATE items SET name = '%s', description = '%s', category = '%s', retailPrice = '%s', salesprice = '%s', specifications = '%s', reviews = '%s', manufacturer = '%s', stock = '%s', stocklimit = '%s', image = '%s' WHERE name = '%s';""" % (fixApost(request.form['name'].encode('utf-8')), fixApost(request.form['description'].encode('utf-8')), fixApost(request.form['category'].encode('utf-8')), request.form['retailPrice'].encode('utf-8'), request.form['salesPrice'].encode('utf-8'), fixApost(request.form['specifications'].encode('utf-8')),  fixApost(request.form['reviews'].encode('utf-8')), fixApost(request.form['manufacturer'].encode('utf-8')), request.form['stock'].encode('utf-8'),  request.form['stockLimit'].encode('utf-8'), request.form['imageName'].encode('utf-8'), fixApost(request.form['name'].encode('utf-8')))
+                  print(cmd) 
+                  cur.execute(cmd)
+                except:
+                  print("Could not update %s" % (fixApost(request.form['name'].encode('utf-8'))))
+                  conn.rollback()
+        
+                conn.commit()  
+        elif request.form['submit'] == 'Add Product':
+            # add new entry into database
+                try:
+                    cmd = """INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews,  manufacturer, stock, rating, stockLimit, image) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '5', '%s');""" % (fixApost(request.form['name'].encode('utf-8')), fixApost(request.form['description'].encode('utf-8')), fixApost(request.form['category'].encode('utf-8')), request.form['retailPrice'].encode('utf-8'), request.form['salesPrice'].encode('utf-8'), fixApost(request.form['specifications'].encode('utf-8')),  fixApost(request.form['reviews'].encode('utf-8')), fixApost(request.form['manufacturer'].encode('utf-8')), request.form['stock'].encode('utf-8'),  request.form['stockLimit'].encode('utf-8'), request.form['imageName'].encode('utf-8'))
+                   
+                    
+                    #print(cmd)
+                    cur.execute(cmd)    
+                except:
+                    print("ERROR inserting into items")
+                    print("INSERT INTO items (name, description, category, retailPrice, salesPrice, specifications, reviews,  manufacturer, stock, rating, stockLimit) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '5');" % (fixApost(request.form['name'].encode('utf-8')), fixApost(request.form['description'].encode('utf-8')), fixApost(request.form['category'].encode('utf-8')), request.form['retailPrice'].encode('utf-8'), request.form['salesPrice'].encode('utf-8'), fixApost(request.form['specifications'].encode('utf-8')),  fixApost(request.form['reviews'].encode('utf-8')), fixApost(request.form['manufacturer'].encode('utf-8')), request.form['stock'].encode('utf-8'),  request.form['stockLimit'].encode('utf-8')))
+                    
+        
+                    conn.rollback()
+        
+                conn.commit()
+    return render_template('addProduct.html', item=results)
+
+
+def fixApost(inString):
+    if "'" in inString:
+        inString = string.replace(inString, "'", "''")
+    return inString       
+  
+
+
+@app.route('/cart', methods=['GET','POST'])
+def cart():
+  
+  if request.method == 'POST':
+    return redirect(url_for('checkout'))
+  
+  return render_template('cart.html')
+  
+  
+@app.route('/checkout', methods=['GET','POST'])
+def checkout():
+  
+  return render_template('checkout.html')
+
+  
+@app.route('/category/<inCategory>/', methods=['GET'])
 def category(inCategory):
-    
+    results = [];
+    print url_for('signup')
     conn = connectToDB()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     print inCategory;
+    if(inCategory == "all"):
+      cmd = """select * from items;"""
+    else:
+      cmd = """select * from items where category = '%s';""" % (inCategory)
+      
     try:
-      cur.execute("""select (name, description, retailprice, salesprice) from items where category = 'parts';""")
+      cur.execute(cmd)
       results = cur.fetchall()
-
+      print results
       
     except:
       print("Error executing select category")
-    return render_template('category.html', items=results)
+      print cmd
+    return render_template('category.html', items=results, cat = inCategory)
 
 # start the server
 if __name__ == '__main__':
